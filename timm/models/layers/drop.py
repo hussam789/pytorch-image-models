505976f3152565591e9114728986a053d85d8c06
hussam789 <hussam789@gmail.com>
2021-01-04 22:14:28 +0200

    skip drop

--- timm/models/layers/drop.py
+++ timm/models/layers/drop.py
@@ -147,14 +147,15 @@ def drop_path(x, drop_prob: float = 0., training: bool = False):
     'survival rate' as the argument.
 
     """
-    if drop_prob == 0. or not training:
-        return x
-    keep_prob = 1 - drop_prob
-    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
-    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
-    random_tensor.floor_()  # binarize
-    output = x.div(keep_prob) * random_tensor
-    return output
+    return x
+#     if drop_prob == 0. or not training:
+#         return x
+#     keep_prob = 1 - drop_prob
+#     shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+#     random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+#     random_tensor.floor_()  # binarize
+#     output = x.div(keep_prob) * random_tensor
+#     return output
 
 
 class DropPath(nn.Module):
